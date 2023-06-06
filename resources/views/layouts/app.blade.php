5a2b0f3acebb247555e2709588a71479075251c7
mauribodnarsky <mbodnarsky74@gmail.com>
2023-06-06 06:42:59 -0300

    cambios inicio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@ Menu</a>
 @guest @else 
 <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
