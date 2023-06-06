e90bc94f14f00532e8ef82cf8373bc62da8a241f
mauribodnarsky <mbodnarsky74@gmail.com>
2023-06-06 06:08:57 -0300

    cambios productos

--- database/migrations/2023_06_06_090417_add_categorias_table.php
+++ database/migrations/2023_06_06_090417_add_categorias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddProductosTable extends Migration
+class AddCategoriasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,15 @@ class AddProductosTable extends Migration
      */
     public function up()
     {
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('descripcion');
-            $table->integer('presentacion');
-            $table->boolean('estado')->default(false);
-            $table->integer('stock')->default(null)->nullable();
-            $table->unsignedBigInteger('categoria_id');
-            $table->foreign('categoria_id')->references('id')->on('categorias');
             $table->foreign('distribuidora_id')->references('id')->on('distribuidoras');
             $table->string('imagen')->default(null)->nullable();
 
             $table->timestamps();
-        });      }
+        });      
+       }
 
     /**
      * Reverse the migrations.
