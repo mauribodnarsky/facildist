f2b75a0ca07d84b9ea0df2e02b630f48c958f86f
mauribodnarsky <mbodnarsky74@gmail.com>
2023-07-18 06:57:43 -0300

    quitar nav

--- resources/views/productos/listado.blade.php
+++ resources/views/productos/listado.blade.php
@@ -64,37 +64,32 @@
         </div>
     </div>
 </div>
-  
-@if(isset($listado) && $listado!=null)
-    <div class="table-responsive">
-    <ol class="list-group list-group-numbered">
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subencabezado</div>
-      Contenido para el elemento de la lista
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subencabezado</div>
-      Contenido para el elemento de la lista
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subencabezado</div>
-      Contenido para el elemento de la lista
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-</ol>
-    </div>
-    @else
-    <div class="card">
-      <div class="card-body text-center">
-        <a href=" " class="btn btn-outline-warning">Crear nuevo producto</a>
-      </div>
-    </div>
+        
+        @if(isset($listado) && $listado!=null)
+            <div class="table-responsive">
+            <ol class="list-group list-group-numbered">
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+            <div class="fw-bold">Subencabezado</div>
+            Contenido para el elemento de la lista
+            </div>
+            <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+            <div class="fw-bold">Subencabezado</div>
+            Contenido para el elemento de la lista
+            </div>
+            <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+            <div class="fw-bold">Subencabezado</div>
+            Contenido para el elemento de la lista
+            </div>
+            <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+        </ol>
+            </div>
+    @endif
 @endsection
