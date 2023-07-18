b1d1c88d1a72ea4acf95c69b64ccd9509d493b7c
mauribodnarsky <mbodnarsky74@gmail.com>
2023-07-18 03:53:32 -0300

    quitar nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Facildist.com.ar') }}</title>
+    <title>{{ config('app.name', 'Facildist') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,7 +21,95 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Facildist') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+
+                        @guest
+                        @else
+                        <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+Menu</a>
+
+
+                        @endguest
+                        @guest
+                         @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+@guest @else 
+<div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <div>
+    </div>
+    <div class="dropdown mt-3">
+      
+     <ul class="nav">
+     <li class="nav-item">
+            <a class="nav-link" href="{{ route('productos.index') }}">{{ __('Productos') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('clientes.index') }}">{{ __('Clientes') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('revistas.index') }}">{{ __('Revistas') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('ofertas.index') }}">{{ __('Ofertas') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('pedidos.index') }}">{{ __('Pedidos') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('distribuidora.perfil') }}">{{ __('Perfil') }}</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}">{{ __('Salir') }}</a>
+    </li>
+     </ul>
+    </div>
+  </div>
+</div>
+@endguest
         <main class="py-4">
             @yield('content')
         </main>
