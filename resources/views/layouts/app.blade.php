2a5aca49226047895b59b701b88c642699389505
mauribodnarsky <mbodnarsky74@gmail.com>
2023-07-18 03:44:59 -0300

    quitar nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Facildist') }}</title>
+    <title>{{ config('app.name', 'Facildist.com.ar') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
