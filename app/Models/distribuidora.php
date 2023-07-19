f980c4117162f126d719f6ced17bb243861c3e5b
mauribodnarsky <mbodnarsky74@gmail.com>
2023-07-19 09:15:57 -0300

    add multiple changes

--- app/Models/distribuidora.php
+++ app/Models/distribuidora.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Categoria;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
@@ -62,5 +62,9 @@ class distribuidora extends Model
 
     public function productos(){
         return $this->hasMany(producto::class);
+    }
+    
+    public function categorias(){
+        return $this->hasMany(Categoria::class);
     }
 }
