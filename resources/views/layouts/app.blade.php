87e21d6eb6ad0defe647f12b429c35bf4d255034
mauribodnarsky <mbodnarsky74@gmail.com>
2023-07-19 07:58:03 -0300

    add logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,32 +24,17 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Facildist') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-
+           
                         @guest
                         @else
                         <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-Menu</a>
+<span class="navbar-toggler-icon"></span></a>
                         @endguest
                     </ul>
                 </div>
-            </div>
         </nav>
+        </div>
+
 @guest @else 
 <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
